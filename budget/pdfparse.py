923a872166b88dde286c498af159600bd0a87bf0
Dzmitry Sasnitski <106316243+dmtrsas@users.noreply.github.com>
2022-06-28 19:55:35 +0300

    minor

--- budget/pdfparse.py
+++ budget/pdfparse.py
@@ -6,7 +6,7 @@ numberofPages = pdf_fileReader.numPages
 page_no = 0
 
 while page_no < numberofPages:
-    parsed_Page = [((pdf_fileReader.getPage(page_no)).extractText()).split('\t')]
+    parsed_Page = [((pdf_fileReader.getPage(page_no)).extractText()).split('\n')]
     with open('parsedresult.txt', 'a') as res:
         for line in parsed_Page:
             res.writelines('\n'.join(line))
